2569a6cb3784e1635e645a71ed92deee5fec033d
Efren Leal <efrenleal19@gmail.com>
2024-04-22 12:29:34 -0500

    debugging'

--- client/src/pages/SavedBooks.jsx
+++ client/src/pages/SavedBooks.jsx
@@ -1,21 +1,53 @@
-import { useState, useEffect } from 'react';
-import {
-  Container,
-  Card,
-  Button,
-  Row,
-  Col
-} from 'react-bootstrap';
-
-// import { getMe, deleteBook } from '../utils/API';
-// import Auth from '../utils/auth';
-// import { removeBookId } from '../utils/localStorage';
+import React from "react";
+import { useQuery } from "@apollo/client";
+import { Container, Card, Button, Row } from 'react-bootstrap';
+import { useMutation } from '../utils/mutations';
+import { GET_ME } from '../utils/queries';
+import { REMOVE_BOOK } from '../utils/mutations';
+import Auth from '../utils/auth';
+import { removeBookId } from "../utils/localStorage";
 
 const SavedBooks = () => {
-  const [userData, setUserData] = useState({});
+  const { data, loading } = useQuery(GET_ME)
+  const userData = data?.me || {}
+
+  const [removeBook] = useMutation(REMOVE_BOOK)
+  const handleDeleteBook = async (bookId) => {
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
+
+    if (!token) {
+      return false;
+    }
+
+    try {
+      const { data } = removeBook({
+        variables: { bookId }
+      });
+
+      removeBookId(bookId);
+    } catch (err) {
+
+    }
+  };
+
+  // import { useState, useEffect } from 'react';
+  // import {
+  //   Container,
+  //   Card,
+  //   Button,
+  //   Row,
+  //   Col
+  // } from 'react-bootstrap';
+
+  // import { getMe, deleteBook } from '../utils/API';
+  // import Auth from '../utils/auth';
+  // import { removeBookId } from '../utils/localStorage';
+
+  // const SavedBooks = () => {
+  //   const [userData, setUserData] = useState({});
 
   // use this to determine if `useEffect()` hook needs to run again
-  const userDataLength = Object.keys(userData).length;
+  // const userDataLength = Object.keys(userData).length;
 
   // useEffect(() => {
   //   const getUserData = async () => {
@@ -67,7 +99,7 @@ const SavedBooks = () => {
   // };
 
   // if data isn't here yet, say so
-  if (!userDataLength) {
+  if (loading) {
     return <h2>LOADING...</h2>;
   }
 
